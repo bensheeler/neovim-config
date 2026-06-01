d21888719a974ca3f2ae1206b9c929110ec175a1
Ben Sheeler <ben.sheeler@openlane.com>
2026-06-01 15:25:16 -0400

    lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        theme = "catppuccin-macchiato",
+        globalstatus = true,
+        disabled_filetypes = {
+          statusline = { "alpha" },
+        },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    },
+  },
+}
